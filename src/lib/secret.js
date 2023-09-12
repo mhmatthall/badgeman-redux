764c3f9649a2b5a1b07034b76e4bacaf12470760
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-09-12 17:48:22 +0100

    Create secret code generator - It's just a substring of an MD5 hash - Functionally it just stops simple abuse

---
+++ src/lib/secret.js
@@ -0,0 +1,9 @@
+import { createHash } from "node:crypto";
+
+export const verify = (id, secret) => {
+  return generate(id) === secret;
+};
+
+export const generate = (id) => {
+  return createHash("md5").update(id).digest("hex").substring(1, 5);
+};
