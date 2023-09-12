96c81800ff62166d7277265c72fd21b37e8ad524
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-09-12 17:49:55 +0100

    Implement login system - Checks for validity on client and server side - TODO: integrate with DB

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,21 +1,47 @@
+import { verify } from "@/lib/secret";
+
 export default async function handler(req, res) {
+  const { body } = req;
+
   // Guard: invalid method
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
   // Guard: invalid body
-  if (!req.body) {
+  if (!body || !body.badgeId || !body.verificationCode) {
     return res.status(400).json({ message: "Invalid request" });
   }
 
-  // Guard: invalid code format
-  if (!/^[0-9a-fA-F]{4}$/.test(req.body)) {
+  // Guard: invalid badge ID format
+  if (!/^[0-9]+$/.test(body.badgeId)) {
     return res.status(400).json({ message: "Invalid request" });
   }
 
-  //debug
-  console.log(`Received badge ID: ${req.body}`);
+  // Guard: invalid verification code format
+  if (!/^[0-9a-fA-F]{4}$/.test(body.verificationCode)) {
+    return res.status(400).json({ message: "Invalid request" });
+  }
 
-  res.status(200).json({ status: "ok" });
+  // Guard: invalid badge ID (not found in DB)
+  // TODO: replace this with a real database query
+  if (body.badgeId !== "1234") {
+    return res
+      .status(404)
+      .json({ message: "Uh-oh, badge not found. Speak to Matt for help!" });
+  }
+
+  // Guard: invalid verification code
+  if (!verify(body.badgeId, body.verificationCode)) {
+    return res
+      .status(400)
+      .json({ message: "That's not the right code. Please try again." });
+  }
+
+  console.log(
+    `Successful ${req.method} at /login -- id: ${body.badgeId} code: ${body.verificationCode}`
+  );
+
+  // Successful request
+  return res.status(200).json({ message: "Login successful" });
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,51 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import Button from "@/components/Button";
 import Card from "@/components/Card";
+import FormWrapper from "@/components/FormWrapper";
+import ProgressIndicator from "@/components/ProgressIndicator";
 import TextField from "@/components/TextField";
 import style from "@/styles/Default.module.scss";
+import axios from "axios";
 import Head from "next/head";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { Controller, useForm } from "react-hook-form";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const {
+    control,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    resetField,
+    setError,
+  } = useForm({
+    defaultValues: {
+      badgeId: "",
+      verificationCode: "",
+    },
+  });
 
-  async function submitBadgeCode(e) {
-    e.preventDefault();
-    setIsLoading(true);
+  // We intercept the form submission event so we can give the user feedback without reloading the page
+  const onSubmit = async (data, event) => {
+    // Prevent browser refresh
+    event.preventDefault();
 
-    try {
-      const response = await fetch("/api/login", {
-        method: "POST",
-        body: e.target.code.value.toLowerCase(),
+    await axios
+      .post("/api/login", data)
+      .then((res) => {
+        // Redirect to the dashboard page if the login was successful
+        if (res.status === 200) {
+          router.push("/editor");
+        }
+      })
+      .catch((err) => {
+        // Show an error message on the form if the login was unsuccessful
+        setError("root.serverError", {
+          type: err.response.status,
+          message: err.response.data.message,
+        });
       });
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  };
 
   return (
     <div className={style.root}>
@@ -30,23 +54,64 @@ export default function Home() {
       </Head>
       <main className={style.main}>
         <Card
-          variant={"outlined"}
-          headline={"Welcome!"}
-          subhead={"Let's setup your digital name badge."}
-          actionsAlignment="stretch"
-          actions={
-            <form onSubmit={submitBadgeCode} className={style.stacked}>
-              <TextField
-                label={"Badge code"}
-                placeholderText={"••••"}
-                trailingIcon={"cancel"}
-              />
-              <button type="submit" disabled={isLoading}>
-                {isLoading ? "Verifying..." : "Start editing"}
-              </button>
-            </form>
-          }
-        />
+          headline="Welcome!"
+          subhead="Let's set up your digital name badge."
+          description="Enter the details as they appear on your badge."
+          variant="outlined"
+        >
+          {isSubmitting && <ProgressIndicator variant="linear" />}
+          <FormWrapper onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="badgeId"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Badge number"
+                  placeholder="e.g. 123"
+                  supportingText={errors.badgeId && "Enter a badge number"}
+                  trailingIconGlyph="cancel"
+                  onTrailingIconClick={() => resetField("badgeId")}
+                  fieldType="text"
+                  htmlName="badgeId"
+                  variant="outlined"
+                  ref={null}
+                />
+              )}
+            />
+            <Controller
+              name="verificationCode"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Secret code"
+                  placeholder="e.g. a1b2"
+                  supportingText={errors.verificationCode && "Enter a code"}
+                  trailingIconGlyph="cancel"
+                  onTrailingIconClick={() => resetField("verificationCode")}
+                  fieldType="text"
+                  htmlName="verificationCode"
+                  variant="outlined"
+                  ref={null}
+                />
+              )}
+            />
+            {errors.root?.serverError && (
+              <div>
+                <p>{errors.root?.serverError.message}</p>
+              </div>
+            )}
+            <Button
+              label="Enter"
+              variant="filled"
+              type="submit"
+              disabled={isSubmitting}
+            />
+          </FormWrapper>
+        </Card>
       </main>
     </div>
   );
