92fad8ed93bb068bd35249cdbee398ab06e22576
Matt <42594962+mhmatthall@users.noreply.github.com>
2023-09-13 21:17:57 +0100

    Implement badge CRUD functions for MongoDB

---
+++ src/lib/db/badgesCrud.js
@@ -0,0 +1,41 @@
+import { generateBlankBadge } from "@/lib/gfx/image";
+
+export async function findBadgebyId(collection, badgeId) {
+  return collection.find({ currentId: parseInt(badgeId) }).toArray();
+}
+
+export async function findBadgebyMac(collection, mac) {
+  return collection.find({ macAddress: mac }).toArray();
+}
+
+export async function updateBadgebyId(collection, badgeId, badgeData) {
+  return collection.updateOne(
+    { currentId: parseInt(badgeId) },
+    {
+      $set: {
+        lastUpdate: new Date(),
+        userData: {
+          ...badgeData,
+        },
+      },
+    }
+  );
+}
+
+export async function createBadgeByMac(collection, macAddress) {
+  const badgeId = (await collection.countDocuments()) + 1;
+  const image = await generateBlankBadge(badgeId);
+
+  return collection.insertOne({
+    macAddress: macAddress,
+    currentId: badgeId,
+    lastUpdate: new Date(),
+    userData: {
+      name: "",
+      pronouns: "",
+      affiliation: "",
+      message: "",
+      image: image,
+    },
+  });
+}
